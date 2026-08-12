418d6cc205c1a444e3b7834873ea62aa62ef016b
shree krishna thapa <220256379+ShreeKrishnaThapa@users.noreply.github.com>
2026-08-12 11:23:22 +0545

    Update transactions.py

--- banking_project/python /bigquery_upload/transactions.py
+++ banking_project/python /bigquery_upload/transactions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from google.cloud import bigquery
-client = bigquery.Client.from_service_account_json(r"D:\m\banking-data-platform456-179067f866a6.json")
+client = bigquery.Client.from_service_account_json(r"service_account_key_file.json")
 df = pd.read_csv(r"C:\Users\mager\Downloads\archive (1)\transactions.csv",  engine='python')
 table_id = "banking-data-platform456.raw.transactions"
 job = client.load_table_from_dataframe(
