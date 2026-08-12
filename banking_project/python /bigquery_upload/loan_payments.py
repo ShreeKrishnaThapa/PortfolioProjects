961308efe95cbbd79cf9d7abe31b444566ca1b47
shree krishna thapa <220256379+ShreeKrishnaThapa@users.noreply.github.com>
2026-08-12 11:19:46 +0545

    Update loan_payments.py

--- banking_project/python /bigquery_upload/loan_payments.py
+++ banking_project/python /bigquery_upload/loan_payments.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from google.cloud import bigquery
-client = bigquery.Client.from_service_account_json(r"D:\m\banking-data-platform456-179067f866a6.json")
+client = bigquery.Client.from_service_account_json(r"service_account_key_file.json")
 df = pd.read_csv(r"D:\animequality force\banking_project\data\raw\loan_payment.csv")
 table_id = "banking-data-platform456.raw.loan_payments"
 job = client.load_table_from_dataframe(
