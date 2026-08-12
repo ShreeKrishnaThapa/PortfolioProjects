3a332a4eb49381624eb84152363ca7df6a852aba
shree krishna thapa <220256379+ShreeKrishnaThapa@users.noreply.github.com>
2026-08-12 11:18:17 +0545

    Add files via upload

---
+++ banking_project/python /bigquery_upload/employees.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from google.cloud import bigquery
+client = bigquery.Client.from_service_account_json(r"D:\m\banking-data-platform456-179067f866a6.json")
+df = pd.read_csv(r"D:\animequality force\banking_project\data\raw\employeed.csv")
+table_id = "banking-data-platform456.raw.employees"
+job = client.load_table_from_dataframe(
+    df,
+    table_id
+)
+
+job.result()
+
+print("Upload Successful!")
